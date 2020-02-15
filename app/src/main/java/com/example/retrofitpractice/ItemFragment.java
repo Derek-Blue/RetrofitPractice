abf78c3924403c8d9b1bf839857331eeda3b137a
a984983 <57126508+a984983@users.noreply.github.com>
2020-02-15 21:17:42 +0800

    加入載圖ProgressBar

--- app/src/main/java/com/example/retrofitpractice/ItemFragment.java
+++ app/src/main/java/com/example/retrofitpractice/ItemFragment.java
@@ -2,9 +2,11 @@ package com.example.retrofitpractice;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
@@ -12,22 +14,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 public class ItemFragment extends Fragment {
 
     private ImageView item_image;
     private TextView name, address, register, status;
+    private ProgressBar progressBar;
 
     private String image_result,name_result,address_result, register_result, status_result;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_item, container, false);
+        final View view = inflater.inflate(R.layout.fragment_item, container, false);
 
+        progressBar = view.findViewById(R.id.progress);
         item_image = view.findViewById(R.id.item_image);
         name = view.findViewById(R.id.name);
         address = view.findViewById(R.id.address);
@@ -36,8 +46,26 @@ public class ItemFragment extends Fragment {
 
         if (TextUtils.isEmpty(image_result)){
             item_image.setImageResource(R.drawable.nopic);
+            progressBar.setVisibility(View.GONE);
         }else {
-            Glide.with(view.getContext().getApplicationContext()).load(image_result).into(item_image);
+            Glide.with(view.getContext().getApplicationContext())
+                    .load(image_result)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            Toast.makeText(view.getContext(), "圖片載入失敗", Toast.LENGTH_SHORT).show();
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            //Toast.makeText(view.getContext(), "成功", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
+                    .error(R.drawable.nopic)
+                    .into(item_image);
         }
 
         name.setText(name_result);
